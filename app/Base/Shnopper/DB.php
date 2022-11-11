c2a8b9b0b1c4f74e09905db8876e16c5e9c35714
stepankarlovec <karlovecstepan@gmail.com>
2022-11-11 11:31:25 +0100

    database basics

--- app/Base/Shnopper/DB.php
+++ app/Base/Shnopper/DB.php
@@ -5,15 +5,23 @@ namespace Shnopper;
 use PDO;
 
 class DB{
+    private $query;
+    private $preparedParams;
+    private $history;
     private $pdo;
 
     public function __construct($host, $dbname, $user, $pass, $options, $charset="utf8mb4")
     {
         $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
         $this->pdo = new PDO($dsn,$user,$pass,$options);
+        $this->query = "";
     }
 
-    public function table(){
-
+    public function table($name){
+        $this->query = "SELECT * FROM " . $name;
+    }
+    public function where($column, $param, $comparer = "="){
+        array_push($this->history, 0);
+        $this->query = $this->query . " WHERE " . $column . $comparer . "?";
     }
 }
--- app/Base/Shnopper/Selection.php
+++ app/Base/Shnopper/Selection.php
@@ -2,12 +2,20 @@
 namespace Shnopper;
 
 class Selection{
+
     public $result;
+
     public $table;
+
     public $params;
+
     public $conditions;
 
-    public function __construct()
+    public function __construct($result, $table, $params, $conditions)
     {
+        $this->result = $result;
+        $this->table = $table;
+        $this->params = $params;
+        $this->conditions = $conditions;
     }
 }
